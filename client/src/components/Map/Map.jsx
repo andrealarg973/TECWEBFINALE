70e2586c8d9e424ae3c0c36c6c0579d6a696b3ac
andrealarg973 <56603394+andrealarg973@users.noreply.github.com>
2024-01-30 12:43:52 +0100

    map work in progress 2

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,118 +1,62 @@
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import 'leaflet-routing-machine';
 
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import './stile.css'
+const MapComponent = () => {
+  const mapRef = useRef(null);
+  const [routeControl, setRouteControl] = useState(null);
 
+  useEffect(() => {
+    if (mapRef.current) {
+      const map = mapRef.current.leafletElement;
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-    iconSize: [24, 36],
-    iconAnchor: [12, 36]
-});
+      const startPoint = [51.505, -0.09]; // Starting point
+      const endPoint = [51.51, -0.1];    // Ending point
 
+      // Create a Leaflet map
+      const leafletMap = L.map(map).setView(startPoint, 13);
 
+      // Add a base map layer
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      }).addTo(leafletMap);
 
-L.Marker.prototype.options.icon = DefaultIcon;
-//console.log(L);
-//L.Routing.control.minimize();
+      // Add a marker at the starting point
+      const startMarker = L.marker(startPoint).addTo(leafletMap);
+      startMarker.bindPopup('Starting Point');
 
-const Map = ({ position, height, zoom, scrollWheelZoom, dragging, draggableMarker, markerRef, notPathMap }) => {
-    /*const map = useMap();
+      // Add a marker at the ending point
+      const endMarker = L.marker(endPoint).addTo(leafletMap);
+      endMarker.bindPopup('Ending Point');
 
-    useEffect(() => {
-        return () => {
-            // Cleanup operations (e.g., remove event listeners, layers)
-            //map.removeLayer(L.marker(waypoints[0]).addTo(map));
-            if (notPathMap) {
-                map.removeLayer(L.marker(position[0], position[1]));
-            } else {
-                for (let i = 0; i < position.length; i += 2) {
-                    map.removeLayer(L.marker(position[i], position[i+1]));
-                }
-            }
-        };
-    }, [map]);*/
+      // Create a routing control
+      const routingControl = L.Routing.control({
+        waypoints: [
+          L.latLng(startPoint),
+          L.latLng(endPoint)
+        ],
+        routeWhileDragging: true
+      });
 
-    if (notPathMap) {
-        return (
-            <MapContainer center={position} dragging={dragging} zoom={zoom} scrollWheelZoom={scrollWheelZoom} style={{ width: "100%", height: height }}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker
-                    draggable={draggableMarker}
-                    position={position}
-                    ref={markerRef}
-                ></Marker>
-            </MapContainer>
-        );
-    } else {
-        const waypoints = [];
-        for (let i = 0; i < position.length; i += 2) {
-            waypoints.push(L.latLng(position[i], position[i + 1]));
-        }
+      routingControl.addTo(leafletMap);
 
-        return (
-            <MapContainer center={waypoints[0]} dragging={dragging} zoom={zoom} scrollWheelZoom={scrollWheelZoom} style={{ width: '100%', height }}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <RoutingMachine waypoints={waypoints} />
-            </MapContainer>
-        );
+      // Set the routing control state
+      setRouteControl(routingControl);
     }
+  }, []);
 
-}
-
-
-const RoutingMap = ({ position, height, zoom }) => {
-    const waypoints = [];
-    for (let i = 0; i < position.length; i += 2) {
-        waypoints.push(L.latLng(position[i], position[i + 1]));
-    }
-
-    return (
-        <MapContainer center={waypoints[0]} zoom={zoom} style={{ width: '100%', height }}>
-            <TileLayer
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }} ref={mapRef}>
+      <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <RoutingMachine waypoints={waypoints} />
-        </MapContainer>
-    );
+      {routeControl}
+    </MapContainer>
+  );
 };
 
-const RoutingMachine = ({ waypoints }) => {
-    const map = useMap();
-
-    useEffect(() => {
-        const control = L.Routing.control({
-            fitSelectedRoutes: true,
-            draggableWaypoints: false,
-            waypoints,
-        }).addTo(map);
-
-        return () => {
-            // Cleanup operations
-            if (control.getPlan()) {
-                control.getPlan().setWaypoints([]); // Clear waypoints
-                map.removeControl(control);
-            }
-        };
-    }, [waypoints, map]);
-
-    return null;
-};
-
-//export default RoutingMap;
-
-
-export default Map;
+export default MapComponent;
